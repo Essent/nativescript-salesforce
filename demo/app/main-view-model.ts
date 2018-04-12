effa1e5aab819e011d5263a1738140886ee89e62
Dennis van den Berg <dennis.vanden.berg@essent.nl>
2018-04-12 13:10:13 +0200

    Added basics for SDK

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,6 +1,8 @@
 import { Observable } from 'tns-core-modules/data/observable';
 import { Salesforce } from 'nativescript-salesforce';
 
+declare const KruxTracker: any;
+
 export class HelloWorldModel extends Observable {
   public message: string;
   private salesforce: Salesforce;
@@ -10,5 +12,8 @@ export class HelloWorldModel extends Observable {
 
     this.salesforce = new Salesforce();
     this.message = this.salesforce.message;
+
+    console.log("KruxTracker: " + KruxTracker);
+    var kt = KruxTracker.sharedEventTrackerWithConfigIdDebugFlagDryRunFlag('abcdefghij', true, false);
   }
 }
