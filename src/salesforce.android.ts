bf2622e87c5061d16bff28e2526f12293f56d4d5
Dennis van den Berg <dennis.vanden.berg@essent.nl>
2018-04-13 16:24:56 +0200

    Added methods for Android

--- src/salesforce.android.ts
+++ src/salesforce.android.ts
@@ -1,8 +1,12 @@
 import { CommonSalesForce } from './salesforce.common';
+import * as application from 'tns-core-modules/application';
+
+declare const com: any;
 
 export class SalesForce implements CommonSalesForce {
 
     private static instance: SalesForce = new SalesForce();
+    private kruxEventAggregator: any = com.krux.androidsdk.aggregator.KruxEventAggregator;
 
     constructor() {
         if (SalesForce.instance) {
@@ -16,6 +20,19 @@ export class SalesForce implements CommonSalesForce {
     }
 
     public initialize(configId: string, debug: boolean): void {
+        let kruxSegmentsCallback = new com.krux.androidsdk.aggregator.KruxSegments({
+            getSegments: (segments: string) => {
+                // Do something with segments.
+            }
+        });
+        this.kruxEventAggregator.initialize(application.android.context, configId, kruxSegmentsCallback , debug);
+    }
 
+    public trackPageView(page: string, pageAttributes: any, userAttributes: any): void {
+        this.kruxEventAggregator.trackPageView(page, pageAttributes, userAttributes)
+    }
+
+    public fireEvent(event: string, eventAttributes: any): void {
+        this.kruxEventAggregator.fire(event, eventAttributes);
     }
 }
