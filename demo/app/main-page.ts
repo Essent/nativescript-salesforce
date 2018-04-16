8ccd4aad42b50b334b71b0f05f90d0e58df6f5b2
Dennis van den Berg <dennis.vanden.berg@essent.nl>
2018-04-16 10:29:58 +0200

    Added buttons to test trackPageView and fireEvent

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,10 +1,31 @@
 import * as observable from 'tns-core-modules/data/observable';
-import * as pages from 'tns-core-modules/ui/page';
-import {HelloWorldModel} from './main-view-model';
+import { SalesForce } from 'nativescript-salesforce';
+
+export interface KeyValue<T> {
+    [key: string]: T;
+}
 
 // Event handler for Page 'loaded' event attached in main-page.xml
-export function pageLoaded(args: observable.EventData) {
-    // Get the event sender
-    let page = <pages.Page>args.object;
-    page.bindingContext = new HelloWorldModel();
+export function pageLoaded(_args: observable.EventData) {
+    SalesForce.getInstance().initialize('abcdefghij', true);
+}
+
+export function trackPageView(args: any) {
+    const attributes: KeyValue<string> = {
+        testKey1: 'My page test value 1',
+        testKey2: 'My page test value 2'
+    };
+    const attributesUser: KeyValue<string> = {
+        testKey1: 'My user test value 1',
+        testKey2: 'My user test value 2'
+    };
+    SalesForce.getInstance().trackPageView('TestPage', attributes, attributesUser);
+}
+
+export function fireEvent(args: any) {
+    const attributes: KeyValue<string> = {
+        testKey1: 'My event test value 1',
+        testKey2: 'My event test value 2'
+    };
+    SalesForce.getInstance().fireEvent('TestEvent', attributes);
 }
